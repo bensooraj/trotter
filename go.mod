e764fe10a5f8aa3cec11a66777c88545478f4c4a
Ben Sooraj <bensooraj@users.noreply.github.com>
2021-07-17 19:04:26 +0530

    dijkstra library, mega flop. Pretty sure I am doing something wrong.
    I will resort to brute-force to see a result first.

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/bensooraj/trotter
 
 go 1.16
+
+require github.com/RyanCarrier/dijkstra v1.1.0
--- graph/graph.go
+++ graph/graph.go
@@ -1,1 +1,53 @@
 package graph
+
+import (
+	"fmt"
+
+	"github.com/RyanCarrier/dijkstra"
+	"github.com/bensooraj/trotter/util"
+)
+
+func FindSingleSourceShortestPath(originCity *City) {
+
+	// https://pkg.go.dev/gonum.org/v1/gonum/graph/path#DijkstraFrom
+
+	d := dijkstra.NewGraph()
+
+	cityStops := []*City{originCity}
+	d.AddVertex(0)
+
+	for i, city := range []*City{
+		originCity.AfricaD.City, originCity.AsiaD.City,
+		originCity.EuropeD.City, originCity.NorthAmericaD.City,
+		originCity.OceaniaD.City, originCity.SouthAmericaD.City,
+	} {
+		if city.ContID != originCity.ContID {
+			cityStops = append(cityStops, city)
+			// Add the cities as vertices
+			d.AddVertex(i + 1)
+		}
+	}
+
+	// Create edges and calculate the weights between them
+	for i, sourceCity := range cityStops {
+		for j, destinationCity := range cityStops {
+			if sourceCity.ID != destinationCity.ID {
+				d.AddArc(i, j, int64(util.GetDistanceFromLatLonInKm(
+					sourceCity.Lat, sourceCity.Lon,
+					destinationCity.Lat, destinationCity.Lon,
+				)))
+			}
+		}
+	}
+
+	d.ExportToFile("d.txt")
+
+	// Find the shortest path starting from the origin back to the origin
+	best, err := d.Shortest(0, 5)
+	if err != nil {
+		fmt.Println("ERROR: ", err)
+		return
+	}
+
+	fmt.Println(best.Path, best.Distance)
+}
